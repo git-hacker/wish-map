b1d0b4d2f76c517ab68ebd9eab182422c278114e
Liu Bo <toolib@gmail.com>
2018-01-12 16:14:22 +0800

    add get_wishes view

--- backend/cdwish/urls.py
+++ backend/cdwish/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from wish import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^get_wishes/', views.get_wishes),
+
 ]
