fb5016f108b09f76d1d26aea76156bb83d4f0009
qq yy <qq yy>
2018-01-13 02:03:40 +0800

    add marker tap event

---
+++ src/components/data.js
@@ -0,0 +1,82 @@
+const data = [
+  {
+    'id': 0,
+    'user_id': '54321',
+    'wish_title': '小区遛狗不栓狗',
+    'wish_type': '关爱动物',
+    'wish_desc': '近日，有市民反应不栓狗',
+    'bannar_url': 'banner.jpg',
+    'img_urls': [
+      'img1.png',
+      'img2.png'
+    ],
+    'statu': '解决中',
+    'location': '倪家桥',
+    'stars': 54,
+    'solutions': [
+      {
+        'solution_desc': '倡导牵引绳',
+        'is_choose': false,
+        'solution_user': '李二狗'
+      }
+    ],
+    'geo': {
+      'latitude': 30.5602,
+      'longitude': 104.05486
+    }
+  },
+  {
+    'id': 1,
+    'user_id': '12345',
+    'wish_title': '社区附近公园乱扔垃圾现象严重',
+    'wish_type': '环保回收',
+    'wish_desc': '近日，有市民反应乱扔垃圾现象严重',
+    'bannar_url': 'banner.jpg',
+    'img_urls': [
+      'img1.png',
+      'img2.png'
+    ],
+    'statu': '待解决',
+    'location': '李家沱',
+    'stars': 54,
+    'solutions': [
+      {
+        'solution_desc': '多设置垃圾桶',
+        'is_choose': false,
+        'solution_user': '李二狗'
+      }
+    ],
+    'geo': {
+      'latitude': 30.5702,
+      'longitude': 104.07486
+    }
+  },
+  {
+    'id': 2,
+    'user_id': '12345',
+    'wish_title': '社区附近公园乱扔垃圾现象严重',
+    'wish_type': '环保回收',
+    'wish_desc': '近日，有市民反应乱扔垃圾现象严重',
+    'bannar_url': 'banner.jpg',
+    'img_urls': [
+      'img1.png',
+      'img2.png'
+    ],
+    'statu': '已完成',
+    'location': '李家沱',
+    'stars': 54,
+    'solutions': [
+      {
+        'solution_desc': '多设置垃圾桶',
+        'is_choose': false,
+        'solution_user': '李二狗'
+      }
+    ],
+    'geo': {
+      'latitude': 30.5702,
+      'longitude': 104.07486
+    }
+  }
+]
+
+export default data
